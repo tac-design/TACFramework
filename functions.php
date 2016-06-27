0cc04c940d4a6e2401e2ce7b23129cced5b2e0c1
Lee Bolton <lee@tac-design.co.uk>
2016-06-27 16:14:44 +0100

    Added tac_ prefixes to functions.

--- functions.php
+++ functions.php
@@ -14,18 +14,18 @@ function css_enqueuer() {
         wp_register_style( 'tac_ontawesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css');
     	wp_enqueue_style( 'tac_fontawesome' );
     }
-add_action( 'wp_enqueue_scripts', 'css_enqueuer' );
+add_action( 'wp_enqueue_scripts', 'tac_css_enqueuer' );
 
 
 // Enqueue JS
-function script_enqueuer() {
+function tac_script_enqueuer() {
 	if( ! is_admin() ) {
 		// Carousel
 		wp_register_script( 'tac_carousel', get_stylesheet_directory_uri().'/js/owl.carousel.js', array( 'jquery' ) );
 		wp_enqueue_script( 'tac_carousel' );
 		// matchHeight
-		wp_register_script( 'tac_matchHeight', get_stylesheet_directory_uri().'/js/jquery.matchHeight.js', array( 'jquery' ) );
-		wp_enqueue_script( 'tac_matchHeight' );
+		wp_register_script( 'tac_match_height', get_stylesheet_directory_uri().'/js/jquery.matchHeight.js', array( 'jquery' ) );
+		wp_enqueue_script( 'tac_match_height' );
 		// Modernizr
 		wp_register_script( 'tac_modernizr', get_stylesheet_directory_uri().'/js/modernizr.js', array( 'jquery' ) );
 		wp_enqueue_script( 'tac_modernizr' );
@@ -33,10 +33,11 @@ function script_enqueuer() {
 		wp_register_script( 'tac_site', get_stylesheet_directory_uri().'/js/site.js', array( 'jquery' ) );
 		wp_enqueue_script( 'tac_site' );
 	};
-}	
+}
+add_action( 'wp_enqueue_scripts', 'tac_script_enqueuer' );
 
 // Widgets
-function widgets_init() {
+function tac_widgets_init() {
 	register_sidebar( array(
 		'name' => __( 'Widget Area' ),
 		'id' => 'widget-area',
@@ -47,7 +48,7 @@ function widgets_init() {
 		'after_title' => '</h3>',
 	) );
 }
-add_action( 'widgets_init', 'widgets_init' );
+add_action( 'widgets_init', 'tac_widgets_init' );
 
 // Menus
 register_nav_menus( array(
