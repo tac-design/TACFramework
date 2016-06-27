4e926544809071e873e09a515d7d3ce828559fb3
Lee Bolton <lee@tac-design.co.uk>
2016-06-27 15:33:20 +0100

    Removed IE conditionals from head.

--- header.php
+++ header.php
@@ -1,9 +1,5 @@
 <!DOCTYPE HTML>
-<!--[if IEMobile 7 ]><html class="no-js iem7" manifest="default.appcache?v=1"><![endif]--> 
-<!--[if lt IE 7 ]><html class="no-js ie6" lang="en"><![endif]--> 
-<!--[if IE 7 ]><html class="no-js ie7" lang="en"><![endif]--> 
-<!--[if IE 8 ]><html class="no-js ie8" lang="en"><![endif]--> 
-<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<html class="no-js" lang="en">
 <head>
 	<title><?php wp_title( '|' ); ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
