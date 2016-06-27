91be25537254ddbab467b195e99b3cc3bcc337b8
Lee Bolton <lee@tac-design.co.uk>
2016-06-27 15:35:23 +0100

    Tidied up head.

--- header.php
+++ header.php
@@ -1,17 +1,22 @@
 <!DOCTYPE HTML>
 <html class="no-js" lang="en">
-<head>
-	<title><?php wp_title( '|' ); ?></title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.png"/>
-	<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/apple-touch-icon.png"/>
+	
+	<head>
+		<title><?php wp_title( '|' ); ?></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.png"/>
+		<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/apple-touch-icon.png"/>
 
-	<?php wp_head(); ?>
+		<?php wp_head(); ?>
 
-</head>
-<body <?php body_class(); ?>>
+	</head>
+	
+	<body <?php body_class(); ?>>
 
-<a href="/"><?php bloginfo( 'name' ); ?></a>
-<nav class="main">
-	<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-</nav>
+		<header class="site-head">
+			<a href="/"><?php bloginfo( 'name' ); ?></a>
+		</header>
+
+		<nav class="main-nav">
+			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+		</nav>
