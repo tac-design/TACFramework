092144697e36baa1fdfefd66be903a03f72bfe2a
Lee Bolton <lee@tac-design.co.uk>
2016-07-20 11:30:46 +0100

    Removed CMB - use plugin going forward.

--- lib/tac.framework.posts.php
+++ lib/tac.framework.posts.php
@@ -19,21 +19,4 @@ function tac_create_post_type() {
 
 add_action( 'init', 'tac_create_post_type' );
 
-
-require_once( 'Custom-Meta-Boxes/custom-meta-boxes.php' );
-
-add_filter( 'cmb_meta_boxes', 'tac_metaboxes' );
-
-function tac_metaboxes( array $meta_boxes ) {
-
-	$meta_boxes[] = array(
-		'title' => 'Questions',
-		'pages' => 'page',
-		'fields' => array(
-			array( 'id' => 'field-1',  'name' => 'Text input field', 'type' => 'text', 'repeatable' => true, ),
-		)
-	);
-	return $meta_boxes; 
-}
-
 ?>
