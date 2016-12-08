63d9352ffd20811cc902d0ddc6e66b62b80f959c
Lee Bolton <lee@tac-design.co.uk>
2016-12-08 11:28:31 +0000

    Added indentation.

--- templates/template.php
+++ templates/template.php
@@ -4,8 +4,8 @@
 get_header(); ?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-<h1><?php the_title(); ?></h1>
-<?php the_content(); ?>
+	<h1><?php the_title(); ?></h1>
+	<?php the_content(); ?>
 <?php endwhile; ?>
 
-<?php get_footer(); ?>
+<?php get_footer();
