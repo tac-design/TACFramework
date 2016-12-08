ee0376959b7ccc2b0960ac2aa24ac11cf6a14170
Lee Bolton <lee@tac-design.co.uk>
2016-12-08 15:11:25 +0000

    Fixed bug with incorrectly formatted date/time.

--- parts/archive-post.php
+++ parts/archive-post.php
@@ -5,5 +5,5 @@
  **/ ?>
 
 <h2><a href="<?php esc_url( the_permalink() ); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-<time datetime="<?php echo gmdate("Y-m-d\TH:i:s", $timestamp); ?>"><?php echo gmdate("d/m/Y", $timestamp); ?></time>
+<time datetime="<?php echo the_time("Y-m-d"); ?>">Date: <?php the_time("Y-m-d"); ?></time>
 <?php the_excerpt(); ?>
--- single.php
+++ single.php
@@ -2,7 +2,7 @@
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 	<h2><?php the_title(); ?></h2>
-	<time datetime="<?php echo gmdate("Y-m-d\TH:i:s", $timestamp); ?>"><?php echo gmdate("d/m/Y", $timestamp); ?></time>
+	<time datetime="<?php echo the_time("Y-m-d"); ?>">Date: <?php the_time("Y-m-d"); ?></time>
 	<?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
 	<?php the_content(); ?>
 	<?php comments_template( '', true ); ?>
