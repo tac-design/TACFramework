61aa9b5081627d626ac2c9277f9412d27c42aa3c
Lee Bolton <lee@tac-design.co.uk>
2017-04-25 12:02:01 +0100

    Added more appropriate id and role to footer.

--- footer.php
+++ footer.php
@@ -10,8 +10,10 @@
 
 ?>
 
-		<footer class="site-foot">
-			&copy; <?php echo esc_html( date( 'Y' ) ); ?> <?php bloginfo( 'name' ); ?>
+		<footer id="site-foot" class="site-foot" role="contentinfo">
+			<div class="site-info">
+				<span>&copy; <?php echo esc_html( date( 'Y' ) ); ?> <?php bloginfo( 'name' ); ?></span>
+			</div>
 		</footer>
 
 		<?php wp_footer(); ?>
