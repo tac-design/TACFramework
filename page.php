464b12bb2074bedc80b3a980005f6d29c8c79bf4
Lee Bolton <lee@tac-design.co.uk>
2017-04-25 10:00:16 +0100

    Added part file for displaying content.

--- page.php
+++ page.php
@@ -5,16 +5,29 @@
  * The template for the site footer.
  *
  * @package TAC Framework
- * @since Tac Framework 1.0
+ * @since TAC Framework 1.0
  */
 
-if ( have_posts() ) {
-	while ( have_posts() ) {
-		the_post();
+?>
 
-		the_title();
-		the_content();
-	}
-}
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 
+		<?php
+		while ( have_posts() ) : the_post();
+
+			// Get the relevant template part to display the content.
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; ?>
+
+	</main>
+</div>
+
+<?php
 get_footer();
