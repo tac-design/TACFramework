de02f53a9e65f73cc930d4d44fa88e4579b7f5c7
Lee Bolton <lee@tac-design.co.uk>
2017-04-25 12:13:39 +0100

    Added Gulp and Package files.

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+// Require all packages
+var gulp = require('gulp');
+var less = require('gulp-less');
+var minify = require('gulp-minify-css');
+var notify = require('gulp-notify');
+var plumber = require('gulp-plumber');
+var livereload = require('gulp-livereload');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+var onError = function (err) {
+    notify({
+         title: 'Gulp Task Error',
+         message: 'Check the console.'
+     }).write(err);
+
+     console.log(err.toString());
+     
+     this.emit('end');
+}
+
+gulp.task('less', function() {
+
+	return gulp
+		.src('css/style.less')
+        .pipe(plumber({ errorHandle: onError }))
+        .pipe(less())
+        .on('error', onError)
+        .pipe(autoprefixer('last 2 version', "> 1%", "ie 8"))
+        .pipe(minify())
+        .pipe(gulp.dest('./'))
+        .pipe(livereload())
+        .pipe(notify({
+            title   : 'Gulp Task Complete',
+            message : 'Styles have been compiled'
+        }));
+
+});
+
+// JS Concatination
+gulp.task('js', function() {
+  gulp.src([
+    'js/vendor/matchheight.js',
+    'js/vendor/owl.carousel.js',
+    'js/site.js'])
+    .pipe(concat('site.min.js'))
+    .pipe(minify())
+    .pipe(gulp.dest('js/'))
+    .pipe(notify({ message: 'JS Concatinated'}));
+});
+
+// Watcher
+gulp.task('default', function() {
+    gulp.watch('./css/*.less', ['less']);
+    gulp.watch('./js/*.js', ['js']);
+});
