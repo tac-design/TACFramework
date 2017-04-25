e95d6f58bcdcde66fa2ceee88566d24c52f35222
Lee Bolton <lee@tac-design.co.uk>
2017-04-25 13:51:42 +0100

    Fixed incorrect paths.

--- single.php
+++ single.php
@@ -17,7 +17,7 @@
 		while ( have_posts() ) : the_post();
 
 			// Get the relevant template part to display the content.
-			get_template_part( 'template-parts/content', 'post' );
+			get_template_part( 'parts/content', 'post' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
