3029b0f4aeb543331f8edd5d3ef9943ea97236f3
Lee Bolton <lee@tac-design.co.uk>
2017-04-25 11:20:15 +0100

    Improved usability.

--- 404.php
+++ 404.php
@@ -10,10 +10,22 @@
 
 get_header(); ?>
 
-<h1>404 - Page Not Found!</h1>
-<p>Sorry, the page you were looking for could not be found. It could be that you've typed the address incorrectly or that the page has been removed.</p>
-<h2>Try searching:</h2>
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 
-<?php get_search_form( $echo ); ?>
+		<h1>Page Not Found</h1>
+		<p>Sorry, the page you were looking for could not be found. It could be that you've typed the address incorrectly or that the page has been moved.</p>
+		<p>To help you find what you are looking for, you can:</p>
+		<ul>
+			<li>Return to the <a href="<?php echo esc_html( site_url() ); ?>">home page</a></li>
+			<li>Find out how to contact us</li>
+			<li>Search the website using the form below</li>
+		</ul>
+
+		<?php
+		get_search_form( $echo ); ?>
+
+	</main>
+</div>
 
 <?php get_footer();
