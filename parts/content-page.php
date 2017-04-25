edb9542a52da748fcbd80163cec904cdf51f902a
Lee Bolton <lee@tac-design.co.uk>
2017-04-25 10:03:08 +0100

    Added template part for page content.

---
+++ parts/content-page.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Content Page
+ *
+ * Template part for displaying page content in page.php
+ *
+ * @package TAC Framework
+ * @since TAC Framework 1.0
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
+	</header>
+
+	<div class="entry-content">
+		<?php
+		the_content(); ?>
+	</div>
+
+	<footer class="entry-footer">
+	</footer>
+</article>
