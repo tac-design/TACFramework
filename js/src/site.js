c23b221b205c4131ba33b89a43806025469ce4e9
Lee Bolton <lee@tac-design.co.uk>
2019-04-23 15:45:02 +0100

    Bit more tidying

--- js/src/site.js
+++ js/src/site.js
@@ -1,3 +1,20 @@
-jQuery(document).ready(function($) {
+(function($) {
+
+
+
+
+	/* Simple accordion */
+
+	var allPanels = $('.js-accordion > dd').hide();
+    
+	$('.accordion > dt > a').click(function() {
+		allPanels.slideUp();
+		$(this).parent().next().slideDown();
+		return false;
+	});
+
+
+
+	
 	
 });
