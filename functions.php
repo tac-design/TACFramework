f7e49130ba6dee4d429fe52748547ed5d53dd7f8
Lee Bolton <lee@tac-design.co.uk>
2020-07-10 07:05:30 +0100

    Updated file name

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
 require_once( 'includes/setup.php' );
-require_once( 'includes/theme.php' );
+require_once( 'includes/functions.php' );
 require_once( 'includes/posts.php' );
 require_once( 'includes/queries.php' );
