cb967d980508a3b467d468581e7878be18bce936
Lee Bolton <lee@tac-design.co.uk>
2021-02-22 14:31:35 +0000

    Removed Owl Carousel and added Swiper

--- js/src/site.js
+++ js/src/site.js
@@ -1,3 +1,27 @@
-jQuery(document).ready(function($) {
+$(function() {
 	
+
+	/* Initialise the Micromodal module. */
+	MicroModal.init();
+
+
+	/* Example swiper carousel. */
+	var swiper = new Swiper('.swiper-container', {
+
+		slidesPerView: 'auto',
+		spaceBetween: 10,
+		loop: true,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+
+		// Navigation arrows
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+
+	});
+
 });
