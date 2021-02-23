9002b999523153940b0975e5f49aa1d2697f9198
Lee Bolton <lee@tac-design.co.uk>
2021-02-23 13:10:45 +0000

    Added csslint and updated gulp packages

--- gulpfile.js
+++ gulpfile.js
@@ -8,53 +8,55 @@ var livereload = require('gulp-livereload');
 var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var csscomb = require('gulp-csscomb');
 
 var onError = function (err) {
-    notify({
-         title: 'Gulp Task Error',
-         message: 'Check the console.'
-     }).write(err);
+		notify({
+				 title: 'Gulp Task Error',
+				 message: 'Check the console.'
+		 }).write(err);
 
-     console.log(err.toString());
-     
-     this.emit('end');
+		 console.log(err.toString());
+		 
+		 this.emit('end');
 }
 
 gulp.task('less', function() {
 
-  return gulp
-    .src('css/style.less')
-        .pipe(plumber({ errorHandle: onError }))
-        .pipe(less())
-        .on('error', onError)
-        .pipe(autoprefixer('last 1 version', "> 1%", "ie 10"))
-        .pipe(minify())
-        .pipe(gulp.dest('./'))
-        .pipe(livereload())
-        .pipe(notify({
-            title   : 'Gulp Task Complete',
-            message : 'Styles have been compiled'
-        }));
+	return gulp
+		.src('css/style.less')
+		.pipe(plumber({ errorHandle: onError }))
+		.pipe(less())
+		.on('error', onError)
+		.pipe(autoprefixer('last 1 version', "> 1%", "ie 10"))
+		.pipe(minify())
+		.pipe(csscomb())
+		.pipe(gulp.dest('./'))
+		.pipe(livereload())
+		.pipe(notify({
+			title   : 'Gulp Task Complete',
+			message : 'Styles have been compiled'
+		}));
 
 });
 
 // JS Concatination
 gulp.task('js', function() {
-  gulp.src([
-    'js/vendor/owl.carousel.js',
-    'js/vendor/pushy.js',
-    'js/src/site.js'])
-    .pipe(concat('main.js'))
-    .pipe(uglify('main.js'))
-    .pipe(gulp.dest('js/'))
-    .pipe(notify({ message: 'JS Concatinated'}));
+	gulp.src([
+		'js/vendor/owl.carousel.js',
+		'js/vendor/pushy.js',
+		'js/src/site.js'])
+		.pipe(concat('main.js'))
+		.pipe(uglify('main.js'))
+		.pipe(gulp.dest('js/'))
+		.pipe(notify({ message: 'JS Concatinated'}));
 });
 
 // Watcher
 gulp.task('default', function() {
-    gulp.watch('./css/project/*.less', ['less']);
-    gulp.watch('./css/framework/*.less', ['less']);
-    gulp.watch('./css/vendor/*.less', ['less']);
-    gulp.watch('./css/*.less', ['less']);
-    gulp.watch('./js/src/*.js', ['js']);
+		gulp.watch('./css/project/*.less', ['less']);
+		gulp.watch('./css/framework/*.less', ['less']);
+		gulp.watch('./css/vendor/*.less', ['less']);
+		gulp.watch('./css/*.less', ['less']);
+		gulp.watch('./js/src/*.js', ['js']);
 });
